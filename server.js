c731400874cb697cfbee63b90e3ac288e9195898
Shubham16598 <shubham16598@gmail.com>
2017-09-24 15:40:35 +0530

    heroku part

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require("hbs");
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
@@ -42,4 +42,4 @@ app.get("/about", (req, res) => {
     pageTitle: "About Page in hbs"
   });
 });
-app.listen(3000);
+app.listen(port);
