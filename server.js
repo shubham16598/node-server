d58996d5ba1d99e1b02b68222d4bcaf2b16f981c
Shubham16598 <shubham16598@gmail.com>
2017-09-24 15:00:13 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require("hbs");
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync("Server.log",log+"\n");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintanence.hbs');
+});
+app.use(express.static(__dirname + "/public"));
+hbs.registerHelper('currentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: "Home page in hbs",
+    para: "This is the welcome page great"
+  });
+});
+
+
+app.get("/about", (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "About Page in hbs"
+  });
+});
+app.listen(3000);
